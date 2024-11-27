2577f18aa5e0ec49aee773462cb00a7b584be25c
brandonlma <blam74@my.bcit.ca>
2024-11-26 17:48:59 -0800

    Add to start_story function

--- game.py
+++ game.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import random
 import time
 from operator import truediv
 
 
-def make_board(rows, columns, floors):
+def make_board(rows, columns):
     row = 0
     board = {}
 
@@ -29,7 +28,7 @@ def create_character():
 def fight_temptation():
     enemy = random_enemy()
     int(input(print("You stumble across " + enemy + ". They prepare to attack you! What do you choose to counter?")))
-    guessing_game(enemy)
+    # guessing_game(enemy)
 
 
 def random_enemy(character):
@@ -51,45 +50,50 @@ def random_enemy(character):
     else:
         return "yourself"
 
-def guessing_game(enemy):
 
-
-    if enemy == "yourself":
-        number = random.randInt(1, 2)
-        guess = int(input("You are at a roulette table. Red or black?"))
-    elif enemy == "Ronald McDonald" || enemy == "Colonel Sanders" || enemy == "Mid 2000s Jared Fogle":
-        number = random.randInt(1, 4)
-        guess = int(input("You are at a roulette table. Red or black, even or odd?"))
-    else:
-        number = random.randInt(1, 6)
-        guess = int(input("You are at a roulette table. Red or black, 1-12, 13-24, or 25-36?"))
-
-    if number:
-        return True
-    else:
-        return False
-
-
-def game():
-    character = create_character()
-
-=======
 def start_story(user_name):
 
-    return (f'Hi {user_name}, welcome to the game of life! Lets begin!\n '
-            f'you are 25 years old, fresh out of college hot shot. You just got a job'
-            f'at some big law firm making qauter million dollars a year. \n'
-            f'A year goes by and now your making over 300k a year now, and you\'ve just \n'
-            f'started a relationship with your smoking hot partner. 4 years go by, now your making\n'
-            f'close to a MILLION dollars a year, you\'ve just got married to your partner, '
-            f'and you just bought a new home in a gated community with a massive backyard pool. \n'
-            f'Life \n Is\n Great\n 25 years have gone by now. You have 3 kids, gone through a nasty divorce\n'
-            f'your spouse has cheated on you with the bartender at your local olivegarden, you\'ve \n'
-            f'gambled your money away through an gambling addiction you got through')
+    print('\nHi '+ user_name + ', welcome to the game of life! Lets begin!\n')
+    time.sleep(3)
+    print('You are 25 years old, fresh out of college hot shot. You just got a job\n'
+        'at some big law firm making qauter million dollars a year. \n')
+    time.sleep(5)
+    print('A year goes by and now your making over 300k a year now, and you\'ve just \n'
+        'started a relationship with your smoking hot partner.\n')
+    time.sleep(5)
+    print('4 years go by, now your making close to a MILLION dollars a year.\n'
+          'You\'ve just got married to the love of your life,\n'
+          'and you just bought a new home in a gated community with a massive backyard pool. \n')
+    time.sleep(5)
+    print('LIFE\n')
+    time.sleep(1)
+    print('IS\n')
+    time.sleep(1)
+    print('GREAT\n')
+    time.sleep(1)
+    print('.....\n')
+    time.sleep(4)
+    print('25 years have gone by now.\n') 
+    time.sleep(4)
+    print('You have 3 kids, gone through a nasty divorce.')
+    time.sleep(2)
+    print('Your spouse has cheated on you with the bartender at your local Olive Garden.')
+    time.sleep(2)
+    print('You\'ve gambled your money away through a gambling addiction.')
+    time.sleep(2)
+    print('And most importantly, you\'ve eaten your sadness away and gained 300lbs.')
+    time.sleep(2)
+    print('Your waist size is now 55 and none of your pants fit you anymore.\n')
+    time.sleep(2)
+    print('Your objective is to lose enough weight to be approved a surgical gastric bypass procedure.\n'
+          'Drop your waist size to 40 before Father Time catches up to you.')
+
+
 
 def make_character():
 
     return {'X-Coordinates': 0, 'Y-Coordinates': 0, 'Floor': 1, 'Waist': 55}
+
 
 def make_floors(rows, columns):
 
@@ -99,7 +103,8 @@ def make_floors(rows, columns):
             board[(row, col)] = 'Empty room'
     return board
 
-def level_up(character,rows,columns): # = level
+
+def level_up(character): # = level
     if character['Waist'] == 50:
         character['Floor'] = 2
         make_floors(rows,columns)
@@ -112,24 +117,22 @@ def level_up(character,rows,columns): # = level
 
 
 def game():
-    user_name = input("Please Enter Your Name")
+    row = 6
+    column = 6
+    make_board(row, column)
+    user_name = input("Please Enter Your Name: ")
     start_game = start_story(user_name)
     print(start_game)
     row = 6
     column = 6
     character = make_character()
     level = level_up(character)
->>>>>>> 26e33fde2cc8c218efde27948d38b70c28add3ad
 
 
 def main():
     game()
 
-<<<<<<< HEAD
 
 
 if __name__ == "__main__":
-=======
-if __name__ == '__main__':
->>>>>>> 26e33fde2cc8c218efde27948d38b70c28add3ad
     main()
