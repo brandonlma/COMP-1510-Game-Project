d7c4ed6f2af66d53f9e8f89235f324ee5cec553f
brandonlma <blam74@my.bcit.ca>
2024-12-01 15:02:57 -0800

    Rename character variable to player

--- game.py
+++ game.py
@@ -14,30 +14,29 @@ def game():
     row = 6
     column = 6
     board = initialize.make_board(row, column)
-    direction = movement.get_user_choice()
     initialize.make_board(row, column)
     user_name = input("Please Enter Your Name: ")
     # start_story(user_name)
-    character = initialize.make_character()
-    while character.is_alive and character['level'] <= 3:
-        output_display.describe_current_location(row, column, character)
+    player = initialize.make_character()
+    while player.is_alive and player['level'] <= 3:
+        output_display.describe_current_location(row, column, player)
         direction = movement.get_user_choice()
-        if movement.validate_move(board, character, direction):
-            movement.move_character(character, direction)
-            villain = battle.check_for_villain(character)
+        if movement.validate_move(board, player, direction):
+            movement.move_character(player, direction)
+            villain = battle.check_for_villain(player)
             if villain:
                 # fight_villain()
-                character.is_alive(character)
-            movement.check_if_level_attained(row, column, character)
-            if movement.check_if_level_attained(row, column, character):
-                movement.increase_floor(character)
-                movement.reset_coordinates(character)
-                character.attributes_upgrade(character, character)
+                player.is_alive(player)
+            movement.check_if_level_attained(row, column, player)
+            if movement.check_if_level_attained(row, column, player):
+                movement.increase_floor(player)
+                movement.reset_coordinates(player)
+                player.attributes_upgrade(player, player)
         else:
             print("You can't go past the board")
-    if character.is_alive(character):
-        battle.fight_final_boss(character, user_name)
-        if battle.fight_final_boss(character, user_name):
+    if player.is_alive(player):
+        battle.fight_final_boss(player, user_name)
+        if battle.fight_final_boss(player, user_name):
             print("Congratulations! You won LIPOSUCTION!!")
         else:
             print("You lost. Have fun love handles")
