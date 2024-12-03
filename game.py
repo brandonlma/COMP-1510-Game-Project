f5e3ce2807949c30653ec3435b7dbeaea64da777
brandonlma <blam74@my.bcit.ca>
2024-12-02 20:24:27 -0800

    AAdd check for upgrade

--- game.py
+++ game.py
@@ -15,13 +15,13 @@ def game():
     initialize.make_board(row, column)
     attributes = {'lick': 1}
     user_name = output_display.intro_image()
-    initialize.start_story(user_name)
+    # initialize.start_story(user_name)
     player = initialize.make_character(attributes)
     output_display.display_user_stats(player, user_name)
-    time.sleep(3)
+    time.sleep(2)
     output_display.describe_current_location(row, column, player)
-    time.sleep(3)
-    while user_information.is_alive(player) and player['Level'] <= 3:
+    time.sleep(2)
+    while user_information.is_alive(player) and player['Floor'] <= 3:
         direction = movement.get_user_choice(player)
         if movement.validate_move(board, player, direction):
             if direction == "E":
@@ -31,7 +31,8 @@ def game():
                 movement.move_character(player, direction)
                 villain = battle.check_for_villain(player)
                 if villain:
-                    battle.fight_villain(player)
+                    enemy = battle.random_enemy(player)
+                    battle.fight_villain(player, enemy)
                 movement.check_if_level_attained(row, column, player)
                 if movement.check_if_level_attained(row, column, player):
                     movement.increase_floor(player)
@@ -40,6 +41,8 @@ def game():
                 output_display.describe_current_location(row, column, player)
         else:
             print("You can't go past the board")
+        if player['Kills'] % 5 == 0 and player['Kills'] != 0:
+            user_information.level_upgrade(player)
     if user_information.is_alive(player):
         final_game = battle.fight_final_boss(player, user_name)
         if final_game:
