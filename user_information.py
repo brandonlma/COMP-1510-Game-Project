8e175cd64c28b145400e40adf2d47f2a207e9f6e
brandonlma <blam74@my.bcit.ca>
2024-12-03 21:41:23 -0800

    Add docstrings to user_information.py

--- user_information.py
+++ user_information.py
@@ -1,4 +1,14 @@
 def attributes_upgrade(player, attributes):
+    """
+    Upgrades player's attributes when reaching a new level.
+
+    :param player: a dictionary
+    :param attributes: a list of dictionaries
+    :precondition: player contains all required key:value pairs
+    :precondition: attributes contains individual dictionaries for each ability
+    :postcondition: adds correct dictionary to list
+    :return: None
+    """
     if player['Level'] == 2:
         attributes.append({2: {"bite": 2}})
     if player['Level'] == 3:
@@ -11,12 +21,28 @@ def attributes_upgrade(player, attributes):
 
 
 def is_alive(player):
+    """
+    Determines if player is alive.
+
+    :param player: a dictionary
+    :precondition: player contains all required key:value pairs
+    :precondition: accurately determines if player is alive
+    :return: a Boolean
+    """
     if player['Waist'] < 100:
         return True
     else:
         return False
 
 def level_upgrade(player):
+    """
+    Upgrades player's level.
+
+    :param player: a dictionary
+    :precondition: player contains all required key:value pairs
+    :precondition: accurately increases player's level by 1 and lowers waist size by 10
+    :return: None
+    """
     if player['Level'] < 5:
         print("\nYou've successfully levelled up! -10 Waist size!")
         player['Level'] += 1
