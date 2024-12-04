b2e43fd6f8dd050946b4bcd1e7c1dd6796df3f53
Peter B <jableno672@proton.me>
2024-12-03 20:55:47 -0800

    Create a single unit test for test_objective

---
+++ test_objective.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+import initialize
+
+class Test(TestCase):
+    def test_objective(self):
+        actual = initialize.objective()
+        expected = ('+-------------------------------------------------------+\n'
+                  '| YOUR OBJECTIVE:                                       |\n'
+                  '+-------------------------------------------------------+\n'
+                  '| 1. Reach the 4th level of the hospital without        |\n'
+                  '|    getting too fat.                                   |\n'
+                  '|    STAY BELOW WAIST SIZE 100 OR YOU LOSE              |\n'
+                  '| 2. Resist the temptation of junk food by              |\n'
+                  '|    defeating monsters.                                |\n'
+                  '| 3. Defeat monsters to level up and achieve better     |\n'
+                  '|    attack abilities.                                  |\n'
+                  '| 4. Failed attacks makes you gain waist size!          |\n'
+                  '| 5. WIN AUTOMATICALLY IF YOU REACH WAIST SIZE 30!      |\n'
+                  '+-------------------------------------------------------+\n'
+                  '| TIP: Level up to lower your waist size by 10!         |\n'
+                  '+-------------------------------------------------------+')
+        self.assertEqual(expected, actual)
