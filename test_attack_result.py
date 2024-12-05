6137ba7f1c707ef96a5aff772f55fed88e88394d
Peter B <jableno672@proton.me>
2024-12-04 23:51:03 -0800

    Fix def names in test_attack_result

--- test_attack_result.py
+++ test_attack_result.py
@@ -6,77 +6,77 @@ import battle
 
 class Test(TestCase):
     @patch('random.randint', side_effect=[99])
-    def test_attack_result_floor_one_80(self, _):
+    def test_attack_result_floor_one_ninety_nine(self, _):
         player = {'Floor': 1}
         actual = battle.attack_result(player)
         expected = True
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[99])
-    def test_attack_result_floor_two_80(self, _):
+    def test_attack_result_floor_two_ninety_nine(self, _):
         player = {'Floor': 2}
         actual = battle.attack_result(player)
         expected = True
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[99])
-    def test_attack_result_floor_three_99(self, _):
+    def test_attack_result_floor_three_ninety_nine(self, _):
         player = {'Floor': 3}
         actual = battle.attack_result(player)
         expected = True
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[1])
-    def test_attack_result_floor_one_1(self, _):
+    def test_attack_result_floor_one_one(self, _):
         player = {'Floor': 1}
         actual = battle.attack_result(player)
         expected = False
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[1])
-    def test_attack_result_floor_two_1(self, _):
+    def test_attack_result_floor_two_one(self, _):
         player = {'Floor': 1}
         actual = battle.attack_result(player)
         expected = False
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[1])
-    def test_attack_result_floor_three_1(self, _):
+    def test_attack_result_floor_three_one(self, _):
         player = {'Floor': 1}
         actual = battle.attack_result(player)
         expected = False
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[65])
-    def test_attack_result_floor_one_65(self, _):
+    def test_attack_result_floor_one_sixty_five(self, _):
         player = {'Floor': 1}
         actual = battle.attack_result(player)
         expected = True
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[60])
-    def test_attack_result_floor_two_60(self, _):
+    def test_attack_result_floor_two_sixty(self, _):
         player = {'Floor': 2}
         actual = battle.attack_result(player)
         expected = True
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[55])
-    def test_attack_result_floor_three_55(self, _):
+    def test_attack_result_floor_three_fifty_five(self, _):
         player = {'Floor': 3}
         actual = battle.attack_result(player)
         expected = True
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[60])
-    def test_attack_result_floor_one_60(self, _):
+    def test_attack_result_floor_one_sixty(self, _):
         player = {'Floor': 1}
         actual = battle.attack_result(player)
         expected = False
         self.assertEqual(expected, actual)
 
     @patch('random.randint', side_effect=[60])
-    def test_attack_result_floor_three_60(self, _):
+    def test_attack_result_floor_three_sixty(self, _):
         player = {'Floor': 3}
         actual = battle.attack_result(player)
         expected = True
