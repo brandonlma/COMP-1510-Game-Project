9b56ccb155894bcb7bbbe4ea9055db2a5712c94f
brandonlma <blam74@my.bcit.ca>
2024-12-04 19:01:19 -0800

    Add split functions to game function for map

--- game.py
+++ game.py
@@ -27,7 +27,8 @@ def game():
     output_display.display_user_stats(player, user_name)
     time.sleep(2)
     while user_information.is_alive(player) and player['Floor'] <= 3:
-        output_display.describe_current_location(row, column, player)
+        output_display.describe_current_location(player)
+        output_display.print_map_location(row, column, player)
         direction = movement.get_user_choice()
         if movement.validate_move(board, player, direction):
             movement.validate_move(board, player, direction)
@@ -45,7 +46,7 @@ def game():
                     battle.fight_villain(player, enemy)
                     if player['Kills'] % 3 == 0 and player['Kills'] != 0:
                         user_information.level_upgrade(player)
-                        if player['Level'] <= 3:
+                        if player['Level'] <= 5:
                             print(user_information.attributes_upgrade(player, attributes))
                         time.sleep(2)
                 if movement.check_if_floor_attained(row, column, player):
@@ -58,17 +59,21 @@ def game():
         battle.final_boss_story(user_name)
         final_game = battle.fight_final_boss(player)
         if final_game:
+            time.sleep(1)
             print("Congratulations! You won LIPOSUCTION!!")
         else:
+            time.sleep(1)
             print("You lost. You don't deserve the liposuction.\n")
             time.sleep(2)
             print("Surgical procedure DENIED!")
     else:
         if player['Waist'] >= 100:
+            time.sleep(1)
             print("You lost. You got way too fat.\n")
             time.sleep(2)
             print("Rest in peace.")
         else:
+            time.sleep(1)
             print("WHAT??? YOU WON WITHOUT REACHING THE END GOAL?\n")
             time.sleep(2)
             print("You're too skinny now! You don't need liposuction anymore!\n")
